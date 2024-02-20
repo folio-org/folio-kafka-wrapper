2fe3999f4f9f77c5360ada64e9dee79acf18a620
JavokhirAbdullayev <Javokhir_Abdullaev@epam.com>
2024-02-20 13:38:24 +0500

    add map into kafka producer manager

--- src/main/java/org/folio/kafka/SimpleKafkaProducerManager.java
+++ src/main/java/org/folio/kafka/SimpleKafkaProducerManager.java
@@ -2,9 +2,15 @@ package org.folio.kafka;
 
 import io.vertx.core.Vertx;
 import io.vertx.kafka.client.producer.KafkaProducer;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SimpleKafkaProducerManager implements KafkaProducerManager {
   private static final String PRODUCER_SUFFIX = "_Producer";
+  private final Map<String, KafkaProducer<?, ?>> producers = new ConcurrentHashMap<>();
+  private static final Logger LOGGER = LogManager.getLogger();
 
   private final Vertx vertx;
   private final KafkaConfig kafkaConfig;
@@ -17,6 +23,17 @@ public class SimpleKafkaProducerManager implements KafkaProducerManager {
 
   @Override
   public <K, V> KafkaProducer<K, V> createShared(String producerName) {
-    return KafkaProducer.createShared(vertx, producerName + PRODUCER_SUFFIX, kafkaConfig.getProducerProps());
+    String key = producerName + PRODUCER_SUFFIX;
+    return (KafkaProducer<K, V>) producers.computeIfAbsent(key, k -> KafkaProducer.createShared(vertx, k, kafkaConfig.getProducerProps()));
+  }
+
+  public void closeAllProducers() {
+    producers.forEach((name, producer) -> producer.close().onComplete(ar -> {
+      if (ar.succeeded()) {
+        LOGGER.info("Closed producer: {}", name);
+      } else {
+        LOGGER.warn("Failed to close producer: {}", name);
+      }
+    }));
   }
 }
